6bc6921faeea7c85f25725e27d6547038ad79559
shoptemplate <35912375+shoptemplate@users.noreply.github.com>
2021-12-21 11:41:46 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-$('.show').zoomImage();
 $('.show-small-img:first-of-type').css({'border': 'solid 1px #951b25', 'padding': '2px'})
 $('.show-small-img:first-of-type').attr('alt', 'now').siblings().removeAttr('alt')
 $('.show-small-img').click(function () {
